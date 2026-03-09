151c6bdccd6e85eaad51fe962e0f6eaf5130848a
Deepak-Kumar8824 <deepakkumardeepakkumar06308@gaim.com>
2026-03-09 11:30:46 +0530

    product item component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/cartSlice";
 import { Link } from "react-router-dom";
-
+//
 function ProductItem({ product }) {
 
   const dispatch = useDispatch();
