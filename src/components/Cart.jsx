7fd5673fb0a5f01ae1c5ed0b69993f8b8185716b
Deepak-Kumar8824 <deepakkumardeepakkumar06308@gaim.com>
2026-03-11 12:07:07 +0530

    fixed cart and checkout visibility issue

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import { Link } from "react-router-dom";
-// 
+
 function Cart() {
 
   const cartItems = useSelector(state => state.cart.cartItems);
 
-  const total = cartItems.reduce(
-    (sum,item)=> sum + item.price * item.quantity,
-    0
-  );
-
   return (
+
     <div>
 
       <h2>Your Cart</h2>
@@ -20,18 +16,17 @@ function Cart() {
       {cartItems.length === 0 && <p>Cart is empty</p>}
 
       {cartItems.map(item => (
-        <CartItem key={item.id} item={item} />
+        <CartItem key={item.id} item={item}/>
       ))}
-
-      <h3>Total: ${total}</h3>
 
       {cartItems.length > 0 && (
         <Link to="/checkout">
-          <button>Checkout</button>
+          <button>Go to Checkout</button>
         </Link>
       )}
 
     </div>
+
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setSearch } from "../redux/cartSlice";
-// 
-function Header(){
 
-const dispatch = useDispatch();
+function Header() {
 
-return(
+  const dispatch = useDispatch();
 
-<div>
+  return (
+    <div className="header">
 
-<h2>ShoppyGlobe</h2>
+      <h2>ShoppyGlobe</h2>
 
-<input
-placeholder="Search product"
-onChange={(e)=>dispatch(setSearch(e.target.value))}
-/>
+      <input
+        type="text"
+        placeholder="Search product"
+        onChange={(e)=>dispatch(setSearch(e.target.value))}
+      />
 
-</div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
 
-)
-
+    </div>
+  );
 }
 
-export default Header
+export default Header;
